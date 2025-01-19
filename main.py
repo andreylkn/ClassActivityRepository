6baccda808a177bc5de0eb4ed7eab43bbb582519
Andrei Kruglov <andreylkn@gmail.com>
2025-01-20 12:21:17 +1300

    refactoring

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 # Create class(es) in a retail industry to get information from customer and save it in a file or in a database.
 from Customer import Customer
 from FileManager import FileManager
+from printing_utils import print_file_saved_message, print_getting_information_message
 
 def create_customer():
-    print("Please enter the following information:")
+    print_getting_information_message()
     name = input("Name: ")
     email = input("Email: ")
     phone = input("Phone: ")
     address = input("Address: ")
     return Customer(name, email, phone, address)
 
-def save_to_file(self, customer):
-    with open(self.filename, 'a') as file:
-        file.write(customer.to_string())
-        file.write("-" * 40 + "\n")  # Separator line
-
 def main():
     file_manager = FileManager("customers.txt")
     customer = create_customer()
     file_manager.save(customer)
-    print("Customer information saved to file.")
+    print_file_saved_message()
 
 if __name__ == "__main__":
     main()
-
-
---
+++ printing_utils.py
@@ -0,0 +1,5 @@
+def print_getting_information_message():
+    print("Please enter the following information:")
+
+def print_file_saved_message():
+    print("Customer information saved to file.")
