6c7fc4cc229b6a0fa129059a0e5a967cd018bdf5
Andrei Kruglov <andreylkn@gmail.com>
2025-01-20 12:24:39 +1300

    Refactoring. Moved the classes files into the Classes folder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Create class(es) in a retail industry to get information from customer and save it in a file or in a database.
-from Customer import Customer
-from FileManager import FileManager
+from Classes.Customer import Customer
+from Classes.FileManager import FileManager
 from printing_utils import print_file_saved_message, print_getting_information_message
 
 def create_customer():
