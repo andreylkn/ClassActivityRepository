f4faf569ab1118d12bee887d0911409a71a176a7
Andrei Kruglov <andreylkn@gmail.com>
2025-02-24 14:39:32 +1300

    Adds a simple page based on Flask

---
+++ week12/flask_web_app/static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('alertBtn').addEventListener('click', function() {
+    alert('Hello from JavaScript!');
+});
+
+document.getElementById("ShowMessageButton").addEventListener("click", function () {
+    alert("Some message")
+});
